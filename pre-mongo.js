4224d022f68afb91533567ea5a506536d59bff04
victorbofill <victorm.bofill@gmail.com>
2018-04-12 16:58:37 -0700

    pre-mongo done

---
+++ pre-mongo.js
@@ -0,0 +1,20 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27019/test';
+let db = null;
+MongoClient.connect(url)
+    .then(_db => {
+        db = _db;
+        return db.collection('friends')
+            .find()
+            .toArray();
+    })
+    .then(friends => {
+        console.log(friends);
+    })
+    .catch(err => {
+        console.log('Failure!', err);
+    })
+    .then(() => {
+        db.close();
+    });
