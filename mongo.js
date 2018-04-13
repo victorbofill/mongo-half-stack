dfef7ee3ffd9631ec7f551e4836b11f523586f48
victorbofill <victorm.bofill@gmail.com>
2018-04-12 17:06:02 -0700

    mongo.js done

---
+++ mongo.js
@@ -0,0 +1,23 @@
+const { MongoClient, ObjectId } = require('mongodb');
+
+const url = 'mongodb://localhost:27019/test';
+let client = null;
+MongoClient.connect(url)
+    .then(_client => {
+        client = _client;
+        const db = client.db();
+        return db.collection('friends')
+            .find()
+            .toArray();
+    })
+    .then(friends => {
+        console.log(
+            JSON.stringify(friends, true, 2)
+        );
+    })
+    .catch(err => {
+        console.log('Failure!', err);
+    })
+    .then(() => {
+        client.close();
+    });
